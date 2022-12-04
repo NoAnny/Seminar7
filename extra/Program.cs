9786f021929f0d287e9806d4fe98e95fc0299f52
NoAnnY <noanny@mail.ru>
2022-12-04 14:09:47 +0300

    готовое решение

---
+++ extra/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент.
+
+int[] array = {1,2,3,5,9,11};
+Console.WriteLine($"MEX массива равен {Mex(array)}");
+
+int Mex(int[] array)
+{
+    int mex = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == 0 & array.Length == 1)
+        {
+            mex = -1;
+            break;
+        } 
+        else if (array[i] == 1 & array.Length == 1)
+        {
+            mex = 0;
+            break;
+        } 
+        else if ((array[i] +1) != array[i+1])
+        {
+            mex = (array[i]+1);  
+            break; 
+        } 
+    }
+    return mex; 
+}
